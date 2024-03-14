77a8906c8a32b27d7290208a42c36c7cc8f8396d
Mahdiiii4 <mhdisy0@gmail.com>
2024-03-14 13:29:53 +0100

    Ovning_Generiska_Metod

---
+++ GeneriskaKlasser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+float val1 = 2.345f;
+float val2 = 2.346f;
+
+T Min<T>(T val1, T val2) where T: IComparable<T>
+{
+    int resultat = val1.CompareTo(val2);
+    if(resultat < 0)
+    {
+        return val1;
+    }
+    else if (resultat > 0)
+    {
+        return val2;
+    }
+    else
+    {
+        return val1;
+    }
+}
+
+Console.WriteLine(Min(val1, val2));
